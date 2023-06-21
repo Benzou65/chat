655afe358fa3568e6ab94a4c4e0669bc77c3f184
Lionel Bonzoumet <lionel.bonzoumet@gmail.com>
2023-06-22 01:29:04 +0200

    wip: add Panda CSS

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import { css } from "../styled-system/css";
 import Chat from "./components/Chat";
-
 export default function Home() {
   return (
     <main>
+      <h1 className={css({ fontSize: "2xl", fontWeight: "bold" })}>
+        BenzouGPT
+      </h1>
       <Chat />
     </main>
   );
