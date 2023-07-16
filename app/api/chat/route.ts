8483e308a8d3f5e985633f65e73c5ec38d198869
Lionel Bonzoumet <lionel.bonzoumet@gmail.com>
2023-07-16 20:22:25 +0200

    fix: missing code after merge request

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,6 @@
 import { OpenAIStream, StreamingTextResponse } from 'ai';
 import { Configuration, OpenAIApi } from 'openai-edge';
+import { currentUser } from '@clerk/nextjs';
 
 // Create an OpenAI API client (that's edge friendly!)
 const config = new Configuration({
@@ -11,6 +12,13 @@ const openai = new OpenAIApi(config);
 export const runtime = 'edge';
 
 export async function POST(req: Request) {
+  const user = await currentUser();
+  const authorizedUser = process.env.AUTHORIZED_USER;
+
+  if (!user?.emailAddresses.find((email) => email.emailAddress === authorizedUser)) {
+    return new Response('You are not authorized to use this app.', { status: 401 });
+  }
+
   // Extract the `messages` from the body of the request
   const { messages } = await req.json();
 
@@ -19,6 +27,7 @@ export async function POST(req: Request) {
     model: 'gpt-3.5-turbo',
     stream: true,
     messages,
+    user: user?.id,
   });
   // Convert the response into a friendly text-stream
   const stream = OpenAIStream(response);
